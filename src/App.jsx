63ff31c3c5543664bcafd6144c4c26a0b1b25ac4
mati <matubarovero@gmail.com>
2026-09-22 20:48:57 -0300

    se elimina seccion for:

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import VistaForo from './VistaForo';
 import PanelAdmin from './PanelAdmin';
 import Login from './Login';
 
@@ -7,8 +6,7 @@ export default function App() {
   return (
     <>
       
-      {/* <Login /> */}
-      <VistaForo />
+      <Login /> 
       {/* <PanelAdmin /> */}
     </>
   );
